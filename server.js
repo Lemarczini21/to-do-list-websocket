b5c5ca715abf5193e70ae75d9f1dabe37de56092
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2023-05-06 14:05:17 +0200

    Add lister for addTask and removeTask

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
+});
+
+const io = socket(server);
+
+const tasks = [];
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (data) => {
+    const { task, id } = data;
+    tasks.push({ task, id });
+    socket.broadcast.emit('addTask', data);
+  });
+  socket.on('removeTask', (taskId) => {
+    const index = tasks.findIndex((task) => task.id === taskId);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      io.broadcast.emit('updateTask', tasks);
+    }
+  });
 });
 
 app.use((req, res) => {
